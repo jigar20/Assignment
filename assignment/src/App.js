d76ee46291b0d080fa70dfe11f5ebd8164a6e866
jigar <jigarkataria1994@gmail.com>
2018-07-25 02:43:21 +0530

    all functionality set

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,20 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React, { Component } from 'react';
 import logo from './bankbazaar-logo.png';
-import gender from './male.jpg';
+import female from './Woman.png';
+import male from './men.png';
 import './App.css';
 import ReactSwipe from 'react-swipe';
 import styled from "styled-components";
-import { Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
 
 
 
-
-
+// slide setting
  const Slide = styled.div`
   height: 90vh;
   width: 100vw;
@@ -37,7 +33,7 @@ import AlertTemplate from 'react-alert-template-basic'
 `;
 
 
-
+// main class starts
 class App extends Component {
     constructor() {
     super();
@@ -84,192 +80,168 @@ class App extends Component {
     this.setState({ slideCount: this.reactSwipe.getNumSlides() });
   }
 
-     updateInput(event){
-this.setState({company : event.target.value})
-}
-handleSubmit(){
-console.log('Your input value is: ' + this.state.company)
-if(this.state.company){
-this.next()
-}
-//Send state to the server code
-}
+  updateInput(event){
+    this.setState({company : event.target.value})
+  }
+
+  handleSubmit(){
+    console.log('Your input value is: ' + this.state.company)
+    if(this.state.company){
+      this.next()
+    }
+  }
 
   updateInputDay(event){
-this.setState({day : event.target.value})
-}
+    this.setState({day : event.target.value})
+  }
  
-handleSubmitDate(){
-console.log('Your input value is: ' + this.state.day)
-if(this.state.day){
-this.next()
-}
-//Send state to the server code
-}
-    render() {
-         const swipeOptions = {
+  handleSubmitDate(){
+    console.log('Your input value is: ' + this.state.day)
+    if(this.state.day){
+      this.next()
+    }
+  }
+  render() {
+    const swipeOptions = {
       callback: () => {
         this.getSlideIndex();
         this.getSlideCount();
       },
       speed: 500
     };
-        return ( 
-            <div className = "App" >
-            <header className = "App-header landingpage-sticky" >
-            <div className = "App-logo" > <img src = { logo }  alt = "logo" / ></div>
-           <span className="logo-border"></span>
-           
+    return ( 
+      <div className = "App" >
+        <header className = "App-header landingpage-sticky" >
+          <div className = "App-logo" > <img src = { logo }  alt = "logo" / ></div>
+          <span className="logo-border"></span>
             <nav className="menu-nav">
-            <ul className="menu-left">
-            <li className="loans-menu">
-                <a href="/loan.html" data-target="#loans" title="Apply Loan Online">LOANS</a>                
-            </li>
-            <li className="loans-menu">
-                <a href="/loan.html" data-target="#loans" title="Apply Loan Online">CARDS</a>                
-            </li>
-             <li className="loans-menu">
-                <a href="/loan.html" data-target="#loans" title="Apply Loan Online">INSURANCE</a>                
-            </li>
-             <li className="loans-menu">
-                <a href="/loan.html" data-target="#loans" title="Apply Loan Online">INVESTMENTS</a>                
-            </li>
-            <li className="creditscore-menu">
-                <a href="/credit-score.html?utm_source=bb&amp;utm_medium=display&amp;utm_campaign=HEADER" data-target="#credit-score" title="Free Credit Score">FREE CREDIT SCORE</a>           
-              </li>
-            </ul>
-
-           
-<ul className="menu-right  short-menu-right ">
-<li>
-<a href="javascript:void(0)" className="dropdown-toggle menu-login social-sign-in js-sign-in" executeafter="SocialLogin.init();" rel="nofollow"  title="Login">
-                Login
-            </a>
-</li>
-</ul>
+              <ul className="menu-left">
+                <li className="loans-menu">
+                  <a href="/loan.html" data-target="#loans" title="Apply Loan Online">LOANS</a>                
+                </li>
+                <li className="loans-menu">
+                  <a href="/loan.html" data-target="#loans" title="Apply Loan Online">CARDS</a>                
+                </li>
+                <li className="loans-menu">
+                  <a href="/loan.html" data-target="#loans" title="Apply Loan Online">INSURANCE</a>                
+                </li>
+                <li className="loans-menu">
+                  <a href="/loan.html" data-target="#loans" title="Apply Loan Online">INVESTMENTS</a>                
+                </li>
+                <li className="creditscore-menu">
+                  <a href="/credit-score.html?utm_source=bb&amp;utm_medium=display&amp;utm_campaign=HEADER" data-target="#credit-score" title="Free Credit Score">FREE CREDIT SCORE</a>           
+                </li>
+              </ul>
+              <ul className="menu-right  short-menu-right ">
+                <li>
+                  <a  className="dropdown-toggle menu-login social-sign-in js-sign-in"    title="Login">
+                    Login
+                  </a>
+                </li>
+              </ul>
             </nav>
-
-
-            </header >
-
-
- <div className="eform-strip slide-form-longHeader">
-        <h2 className="js-header-content">Check Your Experian Credit Score</h2>
-    </div>
-             
-              <div className="center">
-         <ReactSwipe ref={el => (this.reactSwipe = el)} className="mySwipe"  swipeOptions={swipeOptions}>
-             <Slide  className="SlideshowViewDesktop_slideContainer_30QVq">
-            <div>
-              <p>Please Select Gender</p>
-              <div className="radio-col">
-              <label class="labelContainer checked">
-             <a>
-             <img onClick={this.next} src={gender} height="230" width="314" styled="display:inline;" />
-             <span className="textLabel">Male</span>
-             </a>
-             </label>
-              <div className="radio-col">
-              <label class="labelContainer checked">
-                <a>
-             <img onClick={this.next} src={gender} height="230" width="314"/>
-             </a>
-             </label>
-             </div>
-             </div>
-            </div>
-            <div className="slideControl">
-              
-              <div className="SlideshowViewDesktop_slideControlNext_3njXD">
-                <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.next}>
-                  <span> A </span>
-                </a>
-              </div>
-            </div>
-          </Slide>
-          <Slide className="SlideshowViewDesktop_slideContainer_30QVq">
-            <div>
-              <p>Please Enter your Birth Date</p>
-              <input type="text" placeholder="Birth date" onChange={this.updateInputDay}  placeholder="DD/MM/YYYY"/>
-                
-              <br/>
-              <br/>
-               <div className="slideContainerHackDesktop">
-               <span> 
-          <button  className="btn btn-large " type="button"id="addpisx" onClick={this.handleSubmitDate}>Submit</button>
-          </span>
-          </div>
-            </div>
-            <div className="slideControl">
-              <div className="SlideshowViewDesktop_slideControlPrev_28nsF">
-
-                <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.prev}>
-                  <span> B </span>
-                </a>
-              </div>
-              <div className="SlideshowViewDesktop_slideControlNext_3njXD">
-                <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.handleSubmitDate}>
-                  <span> A </span>
-                </a>
-              </div>
-            </div>
-          </Slide>
-
-           <Slide className="SlideshowViewDesktop_slideContainer_30QVq">
-           
-             
-             <div>
-             <span>Please Input in company you work or you own.</span>
-             <br/>
-             <br/>
-               <input onChange={this.updateInput} placeholder="Company You work For"/>
-               <br/>
-               <br/>
-               <div className="slideContainerHackDesktop">
-               <span>
-                 <button className="btn btn-large " value="Submit" id="addpix" onClick={this.handleSubmit}>Submit</button>
-                 </span>
-               <div>
-           
-              <br/>
-          </div>
-
-            <div className="slideControl">
-              <div className="SlideshowViewDesktop_slideControlPrev_28nsF">
-
-                <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.prev}>
-                  <span> B </span>
-                </a>
-              </div>
-              <div className="SlideshowViewDesktop_slideControlNext_3njXD">
-                <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.handleSubmit}>
-                  <span> A </span>
-                </a>
-              </div>
-            </div>
-           </div> 
+          </header>
+          <div className="eform-strip slide-form-longHeader">
+            <h2 className="js-header-content">Check Your Experian Credit Score</h2>
+          </div>           
+          <div className="center">
+            <ReactSwipe ref={el => (this.reactSwipe = el)} className="mySwipe"  swipeOptions={swipeOptions}>
+              <Slide  className="SlideshowViewDesktop_slideContainer_30QVq">
+                <div>
+                  <p>Please Select Gender</p>
+                  <div className="radio-col">
+                    <label class="labelContainer checked">
+                      <a  >
+                        <img  src={female}  height="230" width="314" styled="display:inline;" alt="Female"  onClick={this.next}/>
+                          <span class="sprite-gender icon-gender-male"></span>
+                          <span className="textLabel">Female</span>
+                          <input type="radio" name="gender" value="female" class="myRadio"></input>
+                      </a>
+                    </label>
+                  </div>
+                  <div className="radio-col">
+                    <label class="labelContainer checked">
+                      <a >
+                        <img  src={male} height="230" width="314" alt="Male" onClick={this.next}/>
+                        <span className="textLabel">Male</span>
+                        <input type="radio" name="gender" value="male" class="myRadio"></input>
+                      </a>
+                    </label>
+                  </div>
+                  <div className="slideControl">
+                    <div className="SlideshowViewDesktop_slideControlNext_3njXD">
+                      <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.next}>
+                        <span class="prevNext"> &rsaquo; </span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </Slide>
+              <Slide className="SlideshowViewDesktop_slideContainer_30QVq">
+                <div>
+                  <p>Please Enter your Birth Date</p>
+                  <input type="text" placeholder="Birth date" onChange={this.updateInputDay}  placeholder="DD/MM/YYYY"/>
+                  <br/>
+                  <br/>
+                  <div className="slideContainerHackDesktop">
+                    <span> 
+                      <button  className="btn btn-large " type="button"id="addpisx" onClick={this.handleSubmitDate}>Submit</button>
+                    </span>
+                  </div>
+                </div>
+                <div className="slideControl">
+                  <div className="SlideshowViewDesktop_slideControlPrev_28nsF">
+                    <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.prev}>
+                      <span class="prevNext"> &lsaquo; </span>
+                    </a>
+                  </div>
+                  <div className="SlideshowViewDesktop_slideControlNext_3njXD">
+                    <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.handleSubmitDate}>
+                      <span class="prevNext"> &rsaquo; </span>
+                    </a>
+                  </div>
+                </div>
+              </Slide>
+              <Slide className="SlideshowViewDesktop_slideContainer_30QVq">
+                <div>
+                  <span>Please Input in company you work or you own.</span>
+                    <br/>
+                    <br/>
+                  <input onChange={this.updateInput} placeholder="Company You work For"/>
+                    <br/>
+                    <br/>
+                  <div className="slideContainerHackDesktop">
+                    <span>
+                      <button className="btn btn-large " value="Submit" id="addpix" onClick={this.handleSubmit}>Submit</button>
+                    </span>
+                  <div>           
+                    <br/>
+                </div>
+                <div className="slideControl">
+                  <div className="SlideshowViewDesktop_slideControlPrev_28nsF">
+                    <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.prev}>
+                      <span class="prevNext">  &lsaquo; </span>
+                    </a>
+                  </div>
+                  <div className="SlideshowViewDesktop_slideControlNext_3njXD">
+                    <a className="SlideshowViewDesktop_slideControlButtonIcon_iILAn" onClick={this.handleSubmit}>
+                      <span class="prevNext"> &rsaquo; </span>
+                    </a>
+                  </div>
+                </div>
+              </div> 
             </div> 
           </Slide>
            <Slide className="SlideshowViewDesktop_slideContainer_30QVq">
             <div>
-              <p>Thank you for your input.You will get your credit-score Soon.</p>
-             
+              <p>Thank you for your input.You will get your credit-score Soon.</p>             
             </div>
           </Slide>
-         </ReactSwipe>
-  <button type="button" onClick={this.prev}>Prev</button>
-          <button type="button" className="btn btn-large" onClick={this.next}>Next</button>
-          <div>
-         
-        </div>
-            </div>
-           
-
-         </div>   
-          
-      
-        );
-    }
+        </ReactSwipe>
+      </div> 
+    </div>     
+);
+}
 }
 
 export default App;
--- assignment/src/index.js
+++ assignment/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
+
 
 ReactDOM.render( < App / > , document.getElementById('root'));
 registerServiceWorker();
